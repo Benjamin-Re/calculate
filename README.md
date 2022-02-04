7b751f7713ece050067a5ec5f189ea99722ec796
Benjamin-Re <renker.benjamin@gmail.com>
2022-02-04 09:05:26 +0100

    Use functions to calculate and update display

--- README.md
+++ README.md
@@ -8,5 +8,11 @@ Calculates basic math operations
 The function operate takes an operator and two numbers and calls
 the appropriate function of the above.
 
-The UI consists of numbers from 0-9, the operators +-*/, a display, and
-a clear button.
+The UI consists of numbers from 0-9, the operators +-*/, a display, an equal and a clear button.
+
+####
+789+
+456-
+123*
+0  /
+C=
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector(".display");
+
 function add(a, b) {
     return a + b;
 }
@@ -15,6 +17,8 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+    a = +a;
+    b = +b;
     let result;
     switch (operator) {
         case '+':
@@ -30,5 +34,81 @@ function operate(operator, a, b) {
             result = divide(a, b);
             break;
     }
-    return result;
+    return Math.floor(result*1000)/1000;
+}
+
+
+
+let a = "";
+let b = "";
+let operator = "";
+let operatorClicked = false;
+let equalLocked = true;
+
+// Add Event listeners to the buttons
+// Save the first number in a 
+// After the operator was clicked save the number in b
+let numbers = document.querySelectorAll(".number");
+numbers.forEach(num => {
+    num.addEventListener("click", () => {
+        if (!operatorClicked) {
+            a += num.textContent;
+            display.textContent = a;
+        } else {
+            b += num.textContent;
+            display.textContent = b;
+            // If b was typed in we can unlock equals
+            equalLocked = false;
+        }
+    })
+})
+
+// When an operator is clicked save a and the operator
+let operators = document.querySelectorAll(".operator");
+operators.forEach(currentOperator => {
+    currentOperator.addEventListener("click", () => {
+        if (!operatorClicked) {
+            // Save display value to a, in case this happens after the equal button was clicked
+            a = display.textContent;
+            operator = currentOperator.textContent;
+            operatorClicked = true;
+        } else {
+            // If the operator was clicked already it should function like equal 
+            equals();
+            a = display.textContent;
+            operator = currentOperator.textContent;
+            operatorClicked = true;
+        }
+    });
+})
+
+// When = clicked calculate with the appropriate method
+let equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+    equals();
+})
+
+function equals() {
+    // Check if number b was typed in otherwise equal stays locked
+    if(!equalLocked){
+        // Save display value to b
+        b = display.textContent;
+        display.textContent = operate(operator, a, b);
+        init();
+        equalLocked = true;
+    }
+}
+
+// When Clear is clicked all is reset
+let clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    init();
+    display.textContent = "0";
+})
+
+function init() {
+    a = "";
+    b = "";
+    operator = "";
+    operatorClicked = false;
 }
